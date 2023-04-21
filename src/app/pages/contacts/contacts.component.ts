43295257886c58ecbfd1e7756b464ca86ac241c9
FRANCESCHETTI Alessandro 1140 <alessandro.franceschetti@csi.it>
2023-04-21 19:14:32 +0200

    alessia belli

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -11,21 +11,15 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 export class ContactsComponent implements OnInit {
 
   faEnvelope = faEnvelope;
-  contacts: Array<{ key: string, name: string, feature: string, email: string }> = [];
+  contacts: Array<{ key: string, name: string, feature: string, email: string, linkedin: string, contact_order: string }> = [];
 
   constructor(private httpClient: HttpClient) { }
 
-  //   {
-  //     "key": "alessiabelli",
-  //     "name": "Alessia Belli",
-  //     "feature": "Social Media Manager & Digital PR",
-  //     "email": "alessiabelli.press@gmail.com"
-  // }
 
   ngOnInit(): void {
-    this.httpClient.get<Array<{ key: string, name: string, feature: string, email: string }>>("assets/data/contacts.json?t=" + new Date().getTime()).subscribe(data => {
+    this.httpClient.get<Array<{ key: string, name: string, feature: string, email: string, linkedin: string, contact_order: string }>>("assets/data/contacts.json?t=" + new Date().getTime()).subscribe(data => {
       console.log(data);
-      this.contacts = data;
+      this.contacts = data.sort((a, b) => { return a.contact_order > b.contact_order ? 1 : -1 });
     })
 
   }
