a8798cd4571d178ee8c17c65635ae1ff811df987
Alessandro Franceschetti <alessandro.franceschetti@csi.it>
2024-05-31 20:05:24 +0200

    collaboration

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -17,9 +17,9 @@ export class ContactsComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.httpClient.get<Array<{ key: string, name: string, feature: string, email: string, linkedin: string, contact_order: string }>>("assets/data/contacts.json?t=" + new Date().getTime()).subscribe(data => {
+    this.httpClient.get<Array<{ key: string, name: string, feature: string, email: string, linkedin: string, contact_order: string, hidden: boolean }>>("assets/data/contacts.json?t=" + new Date().getTime()).subscribe(data => {
       console.log(data);
-      this.contacts = data.sort((a, b) => { return a.contact_order > b.contact_order ? 1 : -1 });
+      this.contacts = data.sort((a, b) => { return a.contact_order > b.contact_order ? 1 : -1 }).filter((c) => !c.hidden);
     })
 
   }
--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -20,10 +20,10 @@ export class TeamComponent implements OnInit {
   constructor(private httpClient: HttpClient, private sanitizer: DomSanitizer, public translate: TranslateService) { }
 
   ngOnInit(): void {
-    this.httpClient.get<Array<{ key: string, name: string, feature: string, email: string, contact_order: number }>>("assets/data/contacts.json?t=" + new Date().getTime()).subscribe(data => {
+    this.httpClient.get<Array<{ key: string, name: string, feature: string, email: string, contact_order: number, hidden: boolean }>>("assets/data/contacts.json?t=" + new Date().getTime()).subscribe(data => {
       console.log(data);
       // this.contacts = data.reverse();
-      this.contacts = data.sort((a, b) => a.contact_order > b.contact_order ? 1 : -1)
+      this.contacts = data.sort((a, b) => a.contact_order > b.contact_order ? 1 : -1).filter((c) => !c.hidden)
       this.contacts.forEach(c => {
         c.showAll = false;
         fetch("assets/content/team/" + c.key + "_short_" + this.translate.currentLang + ".html?t=" + new Date().getTime()).then(res => res.text()).then(data => {
